737ea11253b11c7be50eca75125cd5153a22a461
Jacob Gibellini <jacobgibellini@gmail.com>
2019-06-17 17:55:24 +1000

    Code clean up

--- src/directive-shine.js
+++ src/directive-shine.js
@@ -18,6 +18,11 @@ const shine = (el, binding) => {
         isAnimating = false,
         mouseEntered = false;
 
+    /**
+     * @description
+     * The container the shine elements will sit inside of.
+     * This will be the same dimensions as the main element with the shine effect.
+     */
     const createContainer = () => {
         const container = document.createElement('div');
         container.style.position = 'absolute';
@@ -30,12 +35,16 @@ const shine = (el, binding) => {
         return container;
     }
 
-    const createShineElement = (size, width, _angle) => {
+    /**
+     * @description
+     * Creates a custom shine element.
+     */
+    const createShineElement = (size, width, shineAngle) => {
         const element = document.createElement('div');
         element.style.position = 'absolute';
         element.style.left = '50%' //`calc(${ SHOW_NEGATIVE }50% + ${ -SHINE_WIDTH / 2 }px)`;
         element.style.top = '-100%' //`calc(${ SHOW_NEGATIVE }50% + ${ -SHINE_WIDTH / 2 }px)`;
-        element.style.transform = `rotate(${ - _angle }deg) translate(-50%, -50%)`;
+        element.style.transform = `rotate(${ - shineAngle }deg) translate(-50%, -50%)`;
         element.style.transformOrigin = 'top left';
         element.style.backgroundColor = COLOR;
         element.style.height = width + 'px'; // Not actually width, it's only because we are rotating it that it looks like it.
@@ -45,9 +54,16 @@ const shine = (el, binding) => {
         return element;
     }
 
-    const createExtraShine = (container, size, top, width, _angle) => {
+    /**
+     * @description
+     * Creates another shine element that trails behind
+     * the main shine element to give more of a glossy effect.
+     */
+    const createExtraShine = (props) => {
         if (extraShine) {
-            const extraShineElement = createShineElement(size, width / 4, _angle);
+            const { container, size, top, width, shineAngle } = props;
+            // Extra shine element will always be a quarter of the size of the normal shine element.
+            const extraShineElement = createShineElement(size, width / 4, shineAngle);
             container.appendChild(extraShineElement);
             setTimeout(() => { 
                 extraShineElement.style.top = top 
@@ -55,13 +71,24 @@ const shine = (el, binding) => {
         }
     }
 
-    const shineAnimation = (element, size, height, width, _angle) => {
+    /**
+     * @description
+     * Begins the shine animation by creating the elements
+     * and setting the timeout functions.
+     * This will remove the elements from the DOM once completed.
+     */
+    const shineAnimation = (props) => {
+        // Deconstruct.
+        const { element, size, height, width, shineAngle } = props;
+
+        // Create elements.
         const container = createContainer();
-        const shineElement = createShineElement(size, width, _angle);
+        const shineElement = createShineElement(size, width, shineAngle);
 
         container.appendChild(shineElement);
         element.appendChild(container);
 
+        // Calculate end position.
         const top = `calc(100% + ${ height + width }px)`;
         const shineDelay = DELAY * 1000;
 
@@ -69,7 +96,7 @@ const shine = (el, binding) => {
         setTimeout(() => {
             shineElement.style.top = top;
             // The extra shine behind the main shine.
-            createExtraShine(container, size, top, width, _angle);
+            createExtraShine({ ...props, top, container });
         }, 40 + shineDelay);
 
         // Remove the container & shine elements from the DOM.
@@ -79,43 +106,43 @@ const shine = (el, binding) => {
         }, (TIME * 1000 + SHINE_DELAY) + shineDelay);
     }
 
+    /**
+     * @description
+     * Event handler for when the shine animation should start. 
+     */
     const onEventHandler = (e) => {
+        // Do not fire this handler if shine is already animating or user is still hovering over element.
         if (isAnimating || mouseEntered) {
             return;
         }
-        isAnimating = true;
-        mouseEntered = true;
+        isAnimating = mouseEntered = true;
 
-        let target = e.target;
-        // If the user clicked the currently animating ripple or container, select the parent element.
-        while (target !== el) {
-            target = target.parentElement;
-        }
-
+        // Get main element and shine element details.
+        const target = e.target;
         const width = target.clientWidth;
         const height = target.clientHeight;
         const size = Math.hypot(height, width) * 1.5; // Length of the shine bar.
-        const _angle = angle || SHOW_NEGATIVE + ((Math.atan(height / width) * 180) / Math.PI).toString();
+        // If no angle is provided, get the Hypotenuse of the element.
+        const shineAngle = angle || SHOW_NEGATIVE + ((Math.atan(height / width) * 180) / Math.PI).toString();
 
         // Determine the height of the shine.
         SHINE_WIDTH = shineWidth || (height > width ? height : width) / 4;
 
-        shineAnimation(
-            target,
+        shineAnimation({
+            element: target,
             size,
             height,
-            SHINE_WIDTH,
-            _angle
-        )
-    }
-
-    const onMouseLeaveHandler = (e) => {
-        mouseEntered = false;
+            width: SHINE_WIDTH,
+            shineAngle
+        })
     }
 
     el.style.position = 'relative';
     el.addEventListener(EVENT, onEventHandler);
-    el.addEventListener('mouseleave', onMouseLeaveHandler)
+    el.addEventListener('mouseleave', (e) => {
+        // Reset
+        mouseEntered = false;
+    })
 }
 
 export default shine;
